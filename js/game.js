f864e40fefd7ed03163b3a1f27286c8f4a7eb293
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-03-14 06:49:18 -0500

    Añadi funcionlidad valida posiciones vacias

--- js/game.js
+++ js/game.js
@@ -3,47 +3,45 @@ var board = new Array(9);
 var numJugadas1=0;
 var numJugadas2=0;
 var cudraditos=$('.li');
-var turnoGame1=true;
+var turno=true;
 $.each(cudraditos, function() {$(this).on('click',movimiento)});
 
 function movimiento(evt)
 {
 	var position=$(evt.currentTarget).attr('id');
-	markerPositionJugador1(position);
-	markerPositionJugador2(position);
+	markerPosition(position);
 	//console.log(posicion);
 }
 function isFull(position)
 {
 	return(board[position]==null);
 }
-function markerPositionJugador1(position)
+
+function markerPosition(position)
 {
 	var id = $('#'+position);
-	if(isFull(position))
-	{
-		board[position]="full";
-		//console.log(board[position]);
-		numJugadas1++;
-		console.log("hol");
-		id.find('i').remove();
-		id.append('<i class="fa fa-circle-o" aria-hidden="true"></i>');
-		$('#mov1').text(numJugadas1);
-	}
-}
-function markerPositionJugador2(position)
-{
-	var id = $('#'+position);
-	if(turnoGame1){
+	if(turno){
 		if(isFull(position))
 		{
 			board[position]="full";
-			//console.log(board[position]);
+			numJugadas1++;
+			id.find('i').remove();
+			id.append('<i class="fa fa-circle" aria-hidden="true"></i>');
+			$('#mov1').text(numJugadas1);
+			turno=false;
+		}
+	}
+	else
+	{
+		if(isFull(position))
+		{
+			board[position]="full";
 			numJugadas2++;
-			console.log("hol");
 			id.find('i').remove();
 			id.append('<i class="fa fa-heart" aria-hidden="true"></i>');
-			$('#mov2').text(numJugadas1);
-			turnoGame1=false;
+			$('#mov2').text(numJugadas2);
+			turno=true;
 		}
+
+	}
 }
